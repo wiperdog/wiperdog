8e304cef5c193e97479f6fe5ab21fac12f7ed8a2
nguyenxuanluong-luvina <luongnx512@gmail.com>
2014-08-12 08:27:25 +0700

    Update stopWiperdog.bat

--- bin/stopWiperdog.bat
+++ bin/stopWiperdog.bat
@@ -2,7 +2,7 @@
 set WIPERDOGHOME=%~dp0
 set pidFile=%WIPERDOGHOME%\wiperdog.pid
 echo %pidFile%
-if exist { %pidFile% } (
+if exist %pidFile% (
     FOR /F "tokens=*" %%i IN (%pidFile%) DO set pid=%%i
 	TASKKILL /PID %pid% /T /F  >nul 2>&1
 	if errorlevel 1 (
