0cee4c88ddf40175f36165275133097f2fd4a1e8
nguyenxuanluong-luvina <luongnx512@gmail.com>
2014-08-21 14:04:35 +0700

    Update stop function for #466

--- bin/stopWiperdog.sh
+++ bin/stopWiperdog.sh
@@ -19,16 +19,22 @@ PREFIX=`cd "$dir/.." && pwd`
 # move to the Wiperdog home/bin
 
 cd "$PREFIX/bin"
-pidFile="wiperdog.pid"
-if [ -f wiperdog.pid ];then
-	pid=`cat $pidFile`
-	kill_output=$(kill -9 $pid 2>&1)
-	if [ "$kill_output" = "" ]; then
-		echo "Wiperdog stopped !"
-		rm -rf $pidFile
-	else
-		echo "Failed to stop wiperdog !\n $kill_output"
+
+count=0
+for f in *.pid
+do 
+	if [ -f $f ];then
+		pid=`cat $f`
+		kill_output=$(kill -9 $pid 2>&1)
+		if [ "$kill_output" = "" ]; then
+			echo "Wiperdog process with pid $pid stopped !"
+			rm -rf $f
+			count=$count+1
+		else
+			echo "Failed to stop wiperdog process with pid $pid !\n $kill_output"
+		fi
 	fi
-else 
-	echo "Wiperdog not running"
+done
+if [ $count = 0 ];then
+	echo "Wiperdog not running "
 fi
